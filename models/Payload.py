79d95ca344735c97cd7bc73e6a6238b42b150c20
ccnmagnoo <crcamposn@gmail.com>
2024-08-06 12:31:58 -0400

    payload model formatted

--- models/Payload.py
+++ models/Payload.py
@@ -1,4 +1,28 @@
+from datetime import datetime
+import json
+from typing import Literal
 class Payload:
     """obtain json from"""
-    def __init__(self,params:dict[str,str|int]) -> None:
-        self.params = params
+    def __init__(self,*args:str|int, **kwargs:str) -> None:
+        self.data = {kwargs[i]:args[i] for i in range(len(args))}
+
+    def __str__(self) -> str:
+        return json.dumps(self.data)
+
+
+class SecPayload(Payload):
+    """Payload for affected_detail"""
+    def __init__(self, date:datetime=datetime.now()) -> None:
+        super().__init__(month=date.month,day=date.day,hour=date.hour,anho=date.year)
+
+#cspell: disable
+type REGION = Literal['Metropolitana','Tarapaca','Los Rios','Valparaiso','Biobio',"O'Higgins",'Ñuble','La Araucania','Los Lagos','Aysén']
+class RegPayload(Payload):
+    """Payload for REG_CLIENTS post request"""
+    def __init__(self, region:REGION) -> None:
+        super().__init__(region=region)
+
+if __name__=='__main__':
+    payload = SecPayload()
+    data = payload.data
+    print(data)
--- models/Source.py
+++ models/Source.py
@@ -1,7 +1,9 @@
 from typing import Literal
+import json
+import requests
 
 type SOURCE_ID = Literal['nac_clients','reg_clients','server_hour','affected_agg','affected_data']
-
+type METHOD = Literal['POST','GET']
 class DataSource:
     """class with request data intrinsic"""
     source:dict[SOURCE_ID,str] = {
@@ -13,3 +15,9 @@ class DataSource:
         'affected_total':'https://apps.sec.cl/INTONLINEv1/ClientesAfectados/Get',
         'affected_detail':'https://apps.sec.cl/INTONLINEv1/ClientesAfectados/GetPorFecha',        
     }
+    def getData(self,source:SOURCE_ID,content_type:str='application/json,charset=utf-8',fetch_method:METHOD='POST'  )->json:
+        
+        req = requests.post(self.source[source])
+        
+        return {}
+    
