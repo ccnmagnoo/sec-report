fb491d4606865640e16e23237b80037377225932
ccnmagnoo <crcamposn@gmail.com>
2024-08-06 17:10:43 -0400

    set source object data container

--- models/Payload.py
+++ models/Payload.py
@@ -1,28 +1,32 @@
 from datetime import datetime
 import json
 from typing import Literal
+
+#cspell: disable
+type REGION = Literal['Metropolitana','Tarapaca','Los Rios','Valparaiso','Biobio',\
+    "O'Higgins",'Ñuble','La Araucania','Los Lagos','Aysén']
 class Payload:
     """obtain json from"""
-    def __init__(self,*args:str|int, **kwargs:str) -> None:
-        self.data = {kwargs[i]:args[i] for i in range(len(args))}
+    def __init__(self,*args:str, **kwargs:str) -> None:
+        print('loading keys:',kwargs,'values:',args)
+        self.data = kwargs
 
     def __str__(self) -> str:
+        """json object"""
         return json.dumps(self.data)
-
 
 class SecPayload(Payload):
     """Payload for affected_detail"""
     def __init__(self, date:datetime=datetime.now()) -> None:
         super().__init__(month=date.month,day=date.day,hour=date.hour,anho=date.year)
-
-#cspell: disable
-type REGION = Literal['Metropolitana','Tarapaca','Los Rios','Valparaiso','Biobio',"O'Higgins",'Ñuble','La Araucania','Los Lagos','Aysén']
+        
 class RegPayload(Payload):
     """Payload for REG_CLIENTS post request"""
     def __init__(self, region:REGION) -> None:
-        super().__init__(region=region)
+        super().__init__(region=str(region))
+
 
 if __name__=='__main__':
     payload = SecPayload()
     data = payload.data
-    print(data)
+    print('test',data)
--- models/Source.py
+++ models/Source.py
@@ -1,23 +1,54 @@
 from typing import Literal
 import json
+from pandas import DataFrame
 import requests
+from models.Payload import Payload
 
 type SOURCE_ID = Literal['nac_clients','reg_clients','server_hour','affected_agg','affected_data']
 type METHOD = Literal['POST','GET']
 class DataSource:
     """class with request data intrinsic"""
+    _result = None
     source:dict[SOURCE_ID,str] = {
         #context data
         'nac_clients':'https://apps.sec.cl/INTONLINEv1/ClientesAfectados/GetClientesNacional',
-        'reg_clients': 'https://apps.sec.cl/INTONLINEv1/ClientesAfectados/Get',
+        'reg_clients': 'https://apps.sec.cl/INTONLINEv1/ClientesAfectados/GetClientesRegional',
         'server_hour':'https://apps.sec.cl/INTONLINEv1/ClientesAfectados/GetHoraServer',
         #affected data
         'affected_total':'https://apps.sec.cl/INTONLINEv1/ClientesAfectados/Get',
-        'affected_detail':'https://apps.sec.cl/INTONLINEv1/ClientesAfectados/GetPorFecha',        
+        'affected_detail':'https://apps.sec.cl/INTONLINEv1/ClientesAfectados/GetPorFecha',
     }
-    def getData(self,source:SOURCE_ID,content_type:str='application/json,charset=utf-8',fetch_method:METHOD='POST'  )->json:
+
+    def request(self, source:SOURCE_ID,
+                payload:Payload|None=None,
+                content_type:str='application/json;charset=utf-8',
+                fetch_method:METHOD='POST'  )->json:
+        """web scrap request to www.sec.cl"""
+        req = None
+
+        match fetch_method:
+            case 'POST':
+                req = requests.post(
+                    self.source[source],
+                    data=payload.data,
+                    # headers={'Content-Type':content_type},
+                    timeout=500
+                    )
+            case 'GET':
+                req = requests.get(
+                    self.source[source],
+                    data=payload.data,
+                    # headers={'Content-Type':content_type},
+                    timeout=500
+                    )
+
+        if req.status_code == 200:
+            self._result = req.json()
+            return {"status":200,"result":req.json()}
+        else:
+            return {"status":req.status_code}
         
-        req = requests.post(self.source[source])
-        
-        return {}
-    
+    def dataframe(self)->DataFrame:
+        pass
+
+data_source:DataSource = DataSource()
