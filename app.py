4d2b6dfcc07876fd0c70b9f687348e8dbe19a6bb
ccnmagnoo <crcamposn@gmail.com>
2024-08-27 09:09:32 -0400

    data handler for cleanest etl code

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
 from pprint import pp
 from dash import Dash, html, dcc
+from pandas import DataFrame
 import plotly.express as px
-from models.Source import DataSource as Api
-from models.Payload import RegPayload, AffectedPayload
-from utils.transforms import str2date
-
+from storage.db import storage as data
 
 #set app
 app = Dash(__name__)
 
-#data
-api = Api()
-api.request(source='affected_detail',payload=AffectedPayload())
-events = api.dataframe(FECHA_INT_STR=str2date)
-
-fig = px.bar(events,x='NOMBRE_REGION',y='CLIENTES_AFECTADOS',color='NOMBRE_EMPRESA',barmode='group')
+#graphs
+bar_graph = px.bar(data['affected_detail'],x='NOMBRE_REGION',y='CLIENTES_AFECTADOS',barmode='group')
 
 #app
 app.layout = html.Div(children=[
     html.H1(children='Clientes sin Servicio'),
-    dcc.Graph(id='energy_disconnes',figure=fig)
+    dcc.Graph(id='energy_disconnes',figure=bar_graph)
 ])
 
 #run
