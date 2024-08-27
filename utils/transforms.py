d19dcd515fdeecb67ac1f665804341f3c4c949de
ccnmagnoo <crcamposn@gmail.com>
2024-08-27 15:25:14 -0400

    input param requires date of report

--- utils/transforms.py
+++ utils/transforms.py
@@ -7,8 +7,8 @@ def str2date(date_str:str)->datetime:
     d,m,y = [int(it) for it in date_str.split('/')]
     return datetime(day=d,year=y,month=m)
 
-def deltatime_fmt(event_date:datetime)->str:
+def deltatime_fmt(event_date:datetime,report_date=datetime.now())->str:
     """returns readable delta time"""
-    delta:timedelta = datetime.now()-event_date+timedelta(days=0)
-    return f'{delta.days} dias {delta.seconds/3600:.0f} horas'
+    delta:timedelta = report_date-event_date+timedelta(days=0)
+    return f'{delta.days} d {delta.seconds/3600:.0f} h'
     
